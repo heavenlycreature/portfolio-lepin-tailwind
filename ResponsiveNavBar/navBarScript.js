39fb0d257e58b0c80f92d03745bac5b2f1b0873e
heavenlycreature <lepiinnn@gmail.com>
2022-08-02 14:25:43 +0700

    menghapus beberapa syntax yang tidak perlu

--- ResponsiveNavBar/navBarScript.js
+++ ResponsiveNavBar/navBarScript.js
@@ -1,16 +1,9 @@
 const burger = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav ul');
 const navLink = document.querySelectorAll('nav ul li');
-const button = document.querySelector('.button');
 
 burger.addEventListener('click', (e) => {
     nav.classList.toggle('active');
-// navLink.forEach((el, index) => {
-//         el.style.animation = `navLinks 0.5s ease forward ${index / 7 + 0.3}s`
-//     });
 burger.classList.toggle('menu-active');
 });
-button.addEventListener('click', (e) => {
-    document.body.classList.toggle('dark');
-})
 
