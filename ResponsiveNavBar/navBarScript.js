4362354aff3eb9dc02f84dd0678c9470d4b7657b
Lepin <104693989+heavenlycreature@users.noreply.github.com>
2023-04-15 22:41:29 +0700

    Delete ResponsiveNavBar directory

--- ResponsiveNavBar/navBarScript.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav ul');
-const navLink = document.querySelectorAll('nav ul li');
-
-burger.addEventListener('click', (e) => {
-    nav.classList.toggle('active');
-burger.classList.toggle('menu-active');
-});
-
